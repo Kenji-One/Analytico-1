232fbb0a39ca525514a769204e3995d1377b1f0a
Kenji <vishlano.mivali@gmail.com>
2022-10-03 14:51:08 +0400

    ss

--- js/main.js
+++ js/main.js
@@ -1,111 +1,99 @@
-let myChart = document.getElementById('my-chart').getContext('2d');
+let myChart = document.getElementById("my-chart").getContext("2d");
 
 const monthChart = new Chart(myChart, {
-    type:'bar',
-    data: {
-        labels:['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        datasets:[{
-            label: 'Months',
-            data:[
-                10,
-                20,
-                50,
-                90,
-                70,
-                60,
-                34,
-                20,
-                45,
-                60,
-                20,
-                35
-            ],
-            backgroundColor:[
-              
-                '#D8E3E9'
-            ],
-    
-            borderRadius:[
-                9.1
-            ],
-            hoverOffset: 4,
-            hoverBackgroundColor:   '#00BD9D'
-         
-        }],
-    },
+  type: "bar",
+  data: {
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    datasets: [
+      {
+        label: "Months",
+        data: [10, 20, 50, 90, 70, 60, 34, 20, 45, 60, 20, 35],
+        backgroundColor: ["#D8E3E9"],
 
-   
-    options:{
-         responsive: true,
-      plugins: {
-        legend: {
-          position: 'top',
-        },
-        title: {
-          display: true,
-          text: 'Chart.js Bar Chart'
-        },
-       
+        borderRadius: [9.1],
+        hoverOffset: 4,
+        hoverBackgroundColor: "#00BD9D",
       },
-      scales:{
-        y:{
-            beginAtZero: true,
-            grid: {
-                borderWidth:3,
-                drawBorder:false,
-                drawTicks: false,
-                drawOnChartArea: false,
-                display: false
-            
-            },
-            ticks:{
-                display: false
-            }
-        },
-        x:{
-            grid:{
-                drawTicks: false,
-                display:false,
-                drawBorder:false,
-                drawOnChartArea: false,
-          
-        
-                lineWidth:  5
-            }
-        }
-    }
-  
+    ],
+  },
+
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Chart.js Bar Chart",
+      },
     },
-    
+    scales: {
+      y: {
+        beginAtZero: true,
+        grid: {
+          borderWidth: 3,
+          drawBorder: false,
+          drawTicks: false,
+          drawOnChartArea: false,
+          display: false,
+        },
+        ticks: {
+          display: false,
+        },
+      },
+      x: {
+        grid: {
+          drawTicks: false,
+          display: false,
+          drawBorder: false,
+          drawOnChartArea: false,
 
+          lineWidth: 5,
+        },
+      },
+    },
+  },
 });
- const tooltipLine = {
-        id: 'tooltipLine',
-        beforeDraw: chart => {
-            const myChart = chart.myChart;
-            if(chart.tooltip._active && chart.tooltip._active.length){
-                const myChart = chart.myChart;
-                myChart.save();
-                const activePoint = chart.tooltip._active[0];
-                console.log(activePoint.element.y)
+const tooltipLine = {
+  id: "tooltipLine",
+  beforeDraw: (chart) => {
+    const myChart = chart.myChart;
+    if (chart.tooltip._active && chart.tooltip._active.length) {
+      const myChart = chart.myChart;
+      myChart.save();
+      const activePoint = chart.tooltip._active[0];
+      console.log(activePoint.element.y);
 
-                myChart.beginPath();
-                myChart.setLineDash([5, 7]);
-                myChart.moveTo(activePoint.element.x, chart.chartArea.top);
-                myChart.lineTo(activePoint.element.x, activePoint.element.y);
-                myChart.lineWidth = 2;
-                myChart.strokeStyle = 'grey';
-                myChart.stroke();
-                myChart.restore();
+      myChart.beginPath();
+      myChart.setLineDash([5, 7]);
+      myChart.moveTo(activePoint.element.x, chart.chartArea.top);
+      myChart.lineTo(activePoint.element.x, activePoint.element.y);
+      myChart.lineWidth = 2;
+      myChart.strokeStyle = "grey";
+      myChart.stroke();
+      myChart.restore();
 
-                myChart.beginPath();
-                myChart.moveTo(activePoint.element.x, activePoint.element.y);
-                myChart.lineTo(activePoint.element.x, 350);
-                myChart.lineWidth = 2;
-                myChart.strokeStyle = 'red';
-                myChart.stroke();
-                myChart.restore();
-
-            }
-        }
+      myChart.beginPath();
+      myChart.moveTo(activePoint.element.x, activePoint.element.y);
+      myChart.lineTo(activePoint.element.x, 350);
+      myChart.lineWidth = 2;
+      myChart.strokeStyle = "red";
+      myChart.stroke();
+      myChart.restore();
     }
+  },
+};
